b417f43cd21d4609a86385740db88ce0e3926c74
Douglas Hanley <thesecretaryofwar@gmail.com>
2019-03-15 15:05:11 -0400

    bz2 test

---
+++ bz2test.rs
@@ -0,0 +1,39 @@
+extern crate bzip2;
+
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::path::Path;
+use bzip2::read::BzDecoder;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let fname_inp = &args[1];
+
+    let path = Path::new(fname_inp);
+    let file = File::open(&path).unwrap();
+
+    let bz2_reader = BzDecoder::new(file);
+
+    // let mut buf = String::new();
+    // let total = bz2_reader.read_to_string(&mut buf).unwrap();
+
+    let buf_reader = BufReader::new(bz2_reader);
+    let mut total = 0;
+    let mut lines = 0;
+    for line in buf_reader.lines() {
+        match line {
+            Err(e) => {
+                panic!("Error (line {}, pos {}): {:?}", lines, total, e);
+            },
+            Ok(s) => {
+                lines += 1;
+                total += s.len() + 1;
+            }
+        };
+    }
+    total -= 1;
+
+    println!("Total: {}", total);
+}
--- wikiparse.rs
+++ wikiparse.rs
@@ -4,36 +4,12 @@ extern crate quick_xml;
 use std::env;
 use std::fs::File;
 use std::io::BufReader;
-use std::io::BufRead;
 use std::path::Path;
 
 use bzip2::read::BzDecoder;
 
 use quick_xml::Reader;
 use quick_xml::events::Event;
-
-/* just cat out data */
-
-fn cat(fname_inp: &String) {
-    let path = Path::new(fname_inp);
-    let display = path.display();
-
-    let file = match File::open(&path) {
-        Err(e) => panic!("Couldn't open {}: {:?}", display, e),
-        Ok(file) => file
-    };
-
-    let bz2_reader = BzDecoder::new(file);
-    let buf_reader = BufReader::new(bz2_reader);
-
-    let mut total = 0;
-    for line in buf_reader.lines() {
-        total += line.unwrap().len() + 1;
-        // println!("{}", line.unwrap());
-    }
-
-    println!("Total: {}", total);
-}
 
 /* interpret pages */
 
@@ -100,7 +76,7 @@ fn parse(fname_inp: &String) {
                 }
             },
             Ok(Event::Eof) => break, // exits the loop when reaching end of file
-            Ok(Event::Empty(e)) => {
+            Ok(Event::Empty(_)) => {
                 /*
                 let text = e.unescape_and_decode(&reader).unwrap();
                 println!("Empty event: {}", text);
@@ -122,5 +98,5 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let fname_inp = &args[1];
     println!("Parsing {}", fname_inp);
-    cat(fname_inp);
+    parse(fname_inp);
 }
